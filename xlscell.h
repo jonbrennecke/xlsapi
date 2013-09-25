58122bd82dc99bc7a41d826e2b1ddd26798d325f
Jon Brennecke <jpbrennecke@gmail.com>
2013-09-25 13:40:09 -0700

    Create xlscell.h

---
+++ xlscell.h
@@ -0,0 +1,22 @@
+#ifndef XLCELL_H
+#define XLCELL_H
+
+#include <string>
+
+class XlsCell
+{
+public:
+	XlsCell();
+	~XlsCell();
+	void setValue(std::string);
+	void setColor(std::string);
+	std::string getValue();
+	std::string str();
+
+private:
+	std::string header;
+	std::string footer;
+	std::string value;
+};
+
+#endif
