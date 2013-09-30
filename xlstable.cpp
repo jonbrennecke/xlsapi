5f6aed5da935021d9941d9324edce24a20fa05f1
Jon Brennecke <jpbrennecke@gmail.com>
2013-09-30 01:58:46 -0700

    Update xlstable.cpp

--- xlstable.cpp
+++ xlstable.cpp
@@ -4,52 +4,52 @@
 #include <iostream>
 
 /* Constructor */
-XlsTable::XlsTable() 
+XlsTable::XlsTable()
 {
-	this->expColCount = 0;
-	this->expRowCount = 0;
-	this->fullColCount = 1;
-	this->fullRowCount = 1;
-	this->updateHeader();
-	this->footer = "</Table>\n";
+    this->expColCount = 0;
+    this->expRowCount = 0;
+    this->fullColCount = 1;
+    this->fullRowCount = 1;
+    this->updateHeader();
+    this->footer = "</Table>\n";
 }
 
 /* Destructor */
 XlsTable::~XlsTable(){}
 
 /* Add a row at index, push back everything else */
-XlsRow* XlsTable::addRow(int index) 
+XlsRow* XlsTable::addRow(int index)
 {
-
+    return new XlsRow();
 }
 
-/* 
-*  Overload XlsTable::addRow() with a function that accepts no arguments. 
+/*
+*  Overload XlsTable::addRow() with a function that accepts no arguments.
 *  This method appends an empty XlsRow to the end of XlsTable->rows.
 */
-XlsRow* XlsTable::addRow() 
+XlsRow* XlsTable::addRow()
 {
-	this->expRowCount += 1;
-	this->updateHeader();
-	XlsRow* row = new XlsRow(this);
-	this->rows.push_back(row);
-	return row;
+    this->expRowCount += 1;
+    this->updateHeader();
+    XlsRow* row = new XlsRow(this);
+    this->rows.push_back(row);
+    return row;
 }
 
-/* 
-*  Overload XlsTable::addRow() with a function that accepts an XlsRow* as an argument. 
+/*
+*  Overload XlsTable::addRow() with a function that accepts an XlsRow* as an argument.
 *  This method appends the given XlsRow to the end of XlsTable->rows.
 */
-XlsRow* XlsTable::addRow(XlsRow* row) 
+XlsRow* XlsTable::addRow(XlsRow* row)
 {
-	this->expRowCount += 1;
-	this->updateHeader();
-	this->rows.push_back(row);
-	return row;
+    this->expRowCount += 1;
+    this->updateHeader();
+    this->rows.push_back(row);
+    return row;
 }
 
 /* Add a column at index, push back everything else */
-void XlsTable::addColumn(int index) 
+void XlsTable::addColumn(int index)
 {
 
 }
@@ -61,21 +61,21 @@ void XlsTable::setCell(int i, int j, std::string value)
 }
 
 /* retrieve the worksheets contents as a formatted string */
-std::string XlsTable::str() 
+std::string XlsTable::str()
 {
-	std::string s;
-	for(int i=0;i<this->rows.size();i++)
-	{
-		s += this->rows[i]->str();
-	}
-	return this->header + s + this->footer;
+    std::string s;
+    for(unsigned int i=0;i<this->rows.size();i++)
+    {
+        s += this->rows[i]->str();
+    }
+    return this->header + s + this->footer;
 }
 
 /* update the header with new row and column counts */
 void XlsTable::updateHeader()
 {
-	std::stringstream s;
-	s << "<Table ss:ExpandedColumnCount=\"" << this->expColCount << "\" ss:ExpandedRowCount=\"" << this->expRowCount; 
-	s << "\" x:FullColumns=\"" << this->fullColCount << "\" x:FullRows=\"" << this->fullRowCount << "\">\n";
-	this->header = s.str();
+    std::stringstream s;
+    s << "<Table ss:ExpandedColumnCount=\"" << this->expColCount << "\" ss:ExpandedRowCount=\"" << this->expRowCount;
+    s << "\" x:FullColumns=\"" << this->fullColCount << "\" x:FullRows=\"" << this->fullRowCount << "\">\n";
+    this->header = s.str();
 }
