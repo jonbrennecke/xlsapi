3ec9fc6e55258f7b0efc3659d058a5abc7246169
Jon Brennecke <jpbrennecke@gmail.com>
2013-09-30 01:56:55 -0700

    Update xlscell.cpp

--- xlscell.cpp
+++ xlscell.cpp
@@ -1,14 +1,14 @@
+#include "../rapidxml-1.13/rapidxml.hpp"
 #include "xlscell.h"
-
 #include <string>
 #include <sstream>
-
+#include <vector>
 
 /* Constructor */
-XlsCell::XlsCell() 
+XlsCell::XlsCell()
 {
-	this->header = "\t\t<Cell><Data ss:Type=\"String\">";
-	this->footer = "</Data></Cell>\n";
+    this->header = "\t\t<Cell><Data ss:Type=\"String\">";
+    this->footer = "</Data></Cell>\n";
 }
 
 /* Destructor */
@@ -17,22 +17,22 @@ XlsCell::~XlsCell() {}
 /* set the cell to value */
 void XlsCell::setValue(std::string value)
 {
-	this->value = value;
+    this->value = value;
 }
 
 /* set the cell to value */
 void XlsCell::setColor(std::string color)
 {
-	this->header = "\t\t<Cell><Data ss:Color=\""+color+"\" ss:Type=\"String\">";
+    this->header = "\t\t<Cell ss:StyleID=\""+color+"\"><Data ss:Type=\"String\">";
 }
 
 std::string XlsCell::getValue()
 {
-	return this->value;
+    return this->value;
 }
 
 /* retrieve the cells contents as a formatted string */
-std::string XlsCell::str() 
+std::string XlsCell::str()
 {
-	return this->header + this->value + this->footer;
+    return this->header + this->value + this->footer;
 }
